058d4845e20208802dcd76d4edb35f53b1733670
Joe <joe060683@gmail.com>
2015-08-14 17:04:26 +0800

    practice

---
+++ src/prac/java_switch.java
@@ -0,0 +1,25 @@
+package prac;
+
+public class java_switch {
+	
+	public static void main(String[] args) {
+		String ch = "c";
+		
+		switch(ch)
+		{
+			case "a":
+				System.out.println("AAAA");
+				break;
+			case "b":
+				System.out.println("BBBB");
+				break;
+			case "c":
+				System.out.println("CCCC");
+				break;
+			default:
+				System.out.println("default");
+					
+		}
+	}
+
+}
